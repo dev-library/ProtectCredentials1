9e84e3b607ff6318ac7b934cbe49b5f2642bdaa7
dev.rary@gmail.com <dev.rary@gmail.com>
2023-12-16 22:04:48 +0900

    평문 설정정보 업로드

---
+++ src/main/java/devrary/wild/ProtectCredentials1/ProtectCredentials1Application.java
@@ -0,0 +1,13 @@
+package devrary.wild.ProtectCredentials1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectCredentials1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectCredentials1Application.class, args);
+	}
+
+}
